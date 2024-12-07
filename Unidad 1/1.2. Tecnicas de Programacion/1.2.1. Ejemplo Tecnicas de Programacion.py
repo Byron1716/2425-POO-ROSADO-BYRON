08006aa088bf623b7606a8ab2ee96c0bbdf9a90e
Byron <bx.rosadog@uea.edu.ec>
2024-12-07 08:58:10 -0500

    Update 1.2.1. Ejemplo Tecnicas de Programacion.py

--- Unidad 1/1.2. Tecnicas de Programacion/1.2.1. Ejemplo Tecnicas de Programacion.py
+++ Unidad 1/1.2. Tecnicas de Programacion/1.2.1. Ejemplo Tecnicas de Programacion.py
@@ -100,3 +100,107 @@ personaje_1.atributos()
 personaje_2.atributos()
 
 combate(personaje_1, personaje_2)
+
+#Ejemplo 1
+class Personaje:
+
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida
+
+    def atributos(self):
+        print(self.nombre, ":", sep="")
+        print("·Fuerza:", self.fuerza)
+        print("·Inteligencia:", self.inteligencia)
+        print("·Defensa:", self.defensa)
+        print("·Vida:", self.vida)
+
+    def subir_nivel(self, fuerza, inteligencia, defensa):
+        self.fuerza = self.fuerza + fuerza
+        self.inteligencia = self.inteligencia + inteligencia
+        self.defensa = self.defensa + defensa
+
+    def esta_vivo(self):
+        return self.vida > 0
+
+    def morir(self):
+        self.vida = 0
+        print(self.nombre, "ha muerto")
+
+    def daño(self, enemigo):
+        return self.fuerza - enemigo.defensa
+
+    def atacar(self, enemigo):
+        daño = self.daño(enemigo)
+        enemigo.vida = enemigo.vida - daño
+        print(self.nombre, "ha realizado", daño, "puntos de daño a", enemigo.nombre)
+        if enemigo.esta_vivo():
+            print("Vida de", enemigo.nombre, "es", enemigo.vida)
+        else:
+            enemigo.morir()
+
+
+class Caballero(Personaje):
+
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, espada):
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.espada = espada
+
+    def cambiar_arma(self):
+        opcion = int(input("Elige un arma: (1) Acero Adamantium, daño 9. (2) Vibranium, daño 8"))
+        if opcion == 1:
+            self.espada = 9
+        elif opcion == 2:
+            self.espada = 8
+        else:
+            print("Número de arma incorrecta")
+
+    def atributos(self):
+        super().atributos()
+        print("·Espada:", self.espada)
+
+    def daño(self, enemigo):
+        return self.fuerza * self.espada - enemigo.defensa
+
+
+class Mostruo(Personaje):
+
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, libro):
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.libro = libro
+
+    def atributos(self):
+        super().atributos()
+        print("·Libro:", self.libro)
+
+    def daño(self, enemigo):
+        return self.inteligencia * self.libro - enemigo.defensa
+
+
+def combate(jugador_1, jugador_2):
+    turno = 0
+    while jugador_1.esta_vivo() and jugador_2.esta_vivo():
+        print("\nTurno", turno)
+        print(">>> Acción de ", jugador_1.nombre, ":", sep="")
+        jugador_1.atacar(jugador_2)
+        print(">>> Acción de ", jugador_2.nombre, ":", sep="")
+        jugador_2.atacar(jugador_1)
+        turno = turno + 1
+    if jugador_1.esta_vivo():
+        print("\nHa ganado", jugador_1.nombre)
+    elif jugador_2.esta_vivo():
+        print("\nHa ganado", jugador_2.nombre)
+    else:
+        print("\nEmpate")
+
+
+personaje_1 = Caballero("Adler", 20, 10, 4, 100, 4)
+personaje_2 = Mostruo("Rougue", 5, 15, 4, 100, 3)
+
+personaje_1.atributos()
+personaje_2.atributos()
+
+combate(personaje_1, personaje_2)
